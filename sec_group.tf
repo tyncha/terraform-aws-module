312b72abd4b28107aa4bf83441672a69f03ea94f
bernard <slimflex2@hotmail.com>
2020-11-24 03:56:22 +0000

    new

---
+++ sec_group.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "vpc_task" {
+  name        = "vpc_task"
+  description = "Allow TLS inbound traffic"
+   vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = "${var.tags}"
+}
+
+
---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "cidr_block" {}
+variable "region" {}
+variable "private_cidr_block1" {}
+variable "private_cidr_block2" {}
+variable "private_cidr_block3" {}
+variable "public_cidr_block1" {}
+variable "public_cidr_block2" {}
+variable "public_cidr_block3" {}
+variable "instance_type" {}
+variable "availability_zone" {}
+
+
+
+variable "tags" {
+  type = "map"
+}
+
